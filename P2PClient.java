705482dba8b6731ef618c3b73c42e1ecb5cd8525
davor <davorklju@gmail.com>
2012-12-01 14:28:39 -0500

    Client stuff

--- P2PClient.java
+++ P2PClient.java
@@ -1,5 +1,9 @@
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.util.Scanner;
 
 /**
  * User: davor
@@ -7,83 +11,33 @@ import java.util.*;
  * Time: 12:35 PM
  */
 public class P2PClient {
-    private boolean running;
-    private InetAddress host;
-    private InetSocketAddress server;
-    private volatile ServerSocket inSocket;
-    private DatagramSocket outSock;
+    private final int port;
+    private final InetSocketAddress host;
 
-    public static void main(String args[]) {
-
+    public P2PClient(){
+        port = 40020;
+        Scanner in = new Scanner(System.in);
+        String shost = in.nextLine();
+        host = new InetSocketAddress(shost,port);
     }
 
-    public P2PClient() throws UnknownHostException {
-        this.running = true;
-        host = InetAddress.getLocalHost();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a server to connect to: ");
-        String serverAddr = in.nextLine();
-        server = new InetSocketAddress(serverAddr, 40020);
-    }
-
-    public void run() {
-        Scanner in = new Scanner(System.in);
-        while (running) {
-            System.out.println("enter a number");
-            System.out.println("1: inform and update");
-            System.out.println("2: download");
-            System.out.println("3: rate");
-            System.out.println("4: exit");
-            int input = in.nextInt();
-            try {
-                sendDatagram(input);
-            } catch (SocketException e) {
-                e.printStackTrace();
-            }
+    public void run(){
+        DatagramSocket writeSocket = null;
+        try {
+            writeSocket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
-    }
-
-    public int sendDatagram(int msg) throws SocketException {
-        Scanner in = new Scanner(System.in);
-
-        byte[] data = null;
-        DatagramPacket pack;
-
-        switch (msg) {
-            case 1: {
-                System.out.println("enter a file name");
-                String input = in.nextLine();
-                data = input.getBytes();
-                break;
-            }
-            case 2: {
-                /*
-                * TODO get list of files from server and print them
-                * */
-                System.out.println("select a file name");
-                String input = in.nextLine();
-                break;
-            }
-            case 3: {
-                /*
-                * TODO get list of files from server and print them
-                * */
-                System.out.println("select a file name");
-                String input = in.nextLine();
-                System.out.println("enter a rating from 0 to 5");
-                double r = in.nextDouble();
-                /*
-                *TODO rate the file
-                 */
-                break;
-            }
-            case 4:
-                this.running = false;
-                break;
-            default:
-                System.out.println("did not understand option: " + msg);
-                msg = 0;
+        String msg = "INFORM AFILE.JPEG 0\n";
+        byte[] data = msg.getBytes();
+        DatagramPacket pkt = null;
+        try {
+            pkt = new DatagramPacket(data,data.length,host);
+            writeSocket.send(pkt);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return msg;
     }
 }
